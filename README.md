42a76552d8e839af667aebb0f061914457d0121a
Marcus Walker <mwalke35@yahoo.com>
2022-11-20 23:05:23 -0500

    made javascript code to work through quiz.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # CODE-QUIZ
 
 ## TIMED CODE KNOWLEDGE QUIZ
+-Quiz Game to test knowledge.
+-A online quiz
+-HTML, CSS, Javascript used.
+## Instalation
+
+N/A
+
+## Usage
+This website is used to quiz students on materials learned in the online course. Also to compare students knowledge with other classmates.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-const startButton = document.getElementById('start-btn')
-const answerButtons = document.getElementById('answer-buttons')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-const startContainer = document.querySelector('.controls')
-const quizScore = 0
+var startButton = document.getElementById('start-btn')
+var intbtn = document.querySelector('.intbtn')
+// var answerButtons = document.getElementById('answer-buttons')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+var startContainer = document.querySelector('.controls')
+var initialSubmit = document.querySelector('intbtn')
+var highScores = document.getElementById('hScores')
+var yourScore = document.getElementById('yourScore')
+var quizScore = 0
 let currentQuestionIndex = 0
 let shuffledQuestions
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('countdown');
-var initials = document.getElementById("quizInitials");
+var initials = document.getElementById("scoreContainer");
+var correctIndicator = false
+var points = 0
+var reStart = document.getElementById('goback')
+var quizInitials = document.getElementById('quizInitials')
 
-var message ='Time is up!';
+
+
+var message = 'TIME IS UP!'
 var words = message.split();
-
-// Timer that counts down from 5
+var timeLeft = 15;
+// Timer that counts down from 15
 function countdown() {
-    var timeLeft = 60;
+    
 
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
@@ -28,41 +38,26 @@ function countdown() {
             timeLeft--;
         } else if (timeLeft === 1) {
             // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-            timerEl.textContent = timeLeft + ' second remaining';
-            timeLeft--;
+            timerEl.textContent = timeLeft + 'second remaining';
+            timeLeft--;            
+
         } else {
             // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-            timerEl.textContent = '';
+            timerEl.textContent = 'TIME IS UP!';
             // Use `clearInterval()` to stop the timer
             clearInterval(timeInterval);
             // Call the `displayMessage()` function
-            displayMessage();
-        }
+            // displayMessage();
+            questionContainerElement.classList.add('hide')
+            initials.classList.remove('hide')            
+        } 
     }, 1000);
 }
 
-// Displays the message one word at a time
-function displayMessage() {
-    var wordCount = 0;
+intbtn.addEventListener("click", function(event) {
+    event.preventDefault();
 
-    function hideContainer() {
-
-    }
-
-    // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var msgInterval = setInterval(function () {
-        // If there are no more words left in the message
-        if (words[wordCount] === undefined) {
-            // Use `clearInterval()` to stop the timer
-            clearInterval(msgInterval);
-        } else {
-            // Display one word of the message
-            mainEl.textContent = words[wordCount];
-            wordCount++;
-        }
-    }, 1000);
-}
-
+})
 
 startButton.addEventListener('click', startGame)
 
@@ -71,17 +66,20 @@ function startGame() {
     console.log("start")
     startContainer.classList.add('hide')
     shuffledQuestions = setNextQuestion
-    questionContainerElement.classList.remove('hide')
+    questionContainerElement.classList.remove('hide')    
+
+    
     setNextQuestion()
 }
 
-function setNextQuestion() {
-    showQuestion(questions, currentQuestionIndex)
-}
+  function setNextQuestion() {
+        showQuestion(questions, currentQuestionIndex)
+    }
 
 function showQuestion(question, index) {
     console.log("question", question[index].question)
     questionElement.innerText = question[index].question
+    answerButtonsElement.innerHTML = ""
     question[index].answers.forEach(answer => {
         const button = document.createElement('button')
         button.textContent = answer.text
@@ -89,14 +87,55 @@ function showQuestion(question, index) {
         button.classList.add('btn')
         // button.addEventListener('click', selectAnswer)
         button.onclick = selectAnswer
-        // if (answer.correct) {
-        //   button.dataset.correct = answer.correct
-        // }
+        if (answer.correct) {
+
+        }
         answerButtonsElement.appendChild(button)
     })
 }
 
-function selectAnswer() {
+function selectAnswer(event) {
+    if (this.value == "true") {
+        document.getElementById('correct').textContent = 'Correct'
+        quizScore ++
+        console.log(quizScore)
+        
+    }
+    else {
+        document.getElementById('correct').textContent = 'Wrong'
+        timeLeft -=5
+        
+        
+        
+        
+    }
+    currentQuestionIndex++
+    if (currentQuestionIndex < questions.length) {
+        setNextQuestion()
+        
+
+
+    
+    setTimeout(function(){
+        document.getElementById('correct').textContent = ""        
+    }, 400 )
+    event.preventDefault();
+
+    
+
+   
+    }
+    else {
+        questionContainerElement.classList.add('hide')
+
+        initials.classList.remove('hide')  
+        
+        
+    }
+
+
+
+
     //   const selectedButton = e.target
     console.log("click", this.value)
     //   const correct = selectedButton.dataset.correct
@@ -112,21 +151,99 @@ function selectAnswer() {
     //   }
 }
 
-function setStatusClass(element, correct) {
+function setCorrectDiv(element, correct) {
     clearStatusClass(element)
     if (correct) {
-        element.classList.add('correct')
+        element.value = 'Correct'
+
+       
+
     } else {
-        element.classList.add('wrong')
+        element.value = 'Wrong'
+        
     }
 }
+
+// renderLastPlayer();
+
+// function displayMessage(type, message) {
+//     msgDiv.textContent = message;
+//     msDiv.setAttribute('class', type);
+// }
+
+// function renderLastPlayer() {
+//     var playerName = localStorage.getItem('initials');
+//     var playerScore = localStorage.getItem('quizScore');
+
+//     if (!'initials' || !password) {
+//         return;
+//     }
+
+//     user
+// }
+renderLastRegistered();
+
+function renderLastRegistered() {
+    var quizInitials = localStorage.getItem("initials");
+    var quizScore = localStorage.getItem("quizScore");
+}
+
+var players = []
+const addPlayer = (event) => {
+    event.preventDefault(); // to stop form submitting initials
+    let player = {
+        id: Date.now(),
+        initials : document.getElementById('quizInitials').value,quizScore
+        
+       
+    }
+    players.push(player);
+    document.forms[0].reset();
+
+  
+   
+    // document.querySelector('form').reset(); //to clear the form for the next entries
+
+    // //for display purposes only
+    // console.warn('added', {players} );
+    // let pre = document.querySelector('')
+    // pre.textContent = + JSON.stringify(players);
+
+    //saving to local Storage
+
+    localStorage.setItem('players', JSON.stringify(players) );
+
+
+}
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('button').addEventListener('click', addPlayer);
+    renderLastRegistered();
+});
+
+const restart = (e) => {
+    e.preventDefault();
+    reStart.addEventListener('click', startGame)
+    console.log("not work")
+}
+
+
+
+
+// function setStatusClass(element, correct) {
+//     clearStatusClass(element)
+//     if (correct) {
+//         element.classList.add('correct')
+//     } else {
+//         element.classList.add('wrong')
+//     }
+// }
 
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
 
-const questions = [
+var questions = [
     {
         question: 'What is 2 + 2?',
         answers: [
